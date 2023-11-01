0002264598856bebbebd2048a3c0ef2fe47f36b3
Helena <helena.wiklund@live.se>
2023-11-01 10:02:11 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Card } from "lib/Card";
+
+export const App = () => {
+  return (
+    <Card
+      coverimg="http://www.fillmurray.com/284/196"
+      title="this is the title!"
+      secondaryText="Blablablabla"
+      thumbnailUrl="http://www.fillmurray.com/460/300"
+    />
+  );
+};
